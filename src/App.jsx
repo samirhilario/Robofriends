cbacec931dbdd2b4927230c3a896336f45361f8d
Samir Hilario <samhilario1137@yahoo.com>
2024-08-24 20:31:29 -0400

    vite/tailwind boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Footer from './assets/components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <>
+            <h1 className="text-red-500">
+                Hello World
+            </h1>
+        </>
+    )
+}
+
+export default Footer;
